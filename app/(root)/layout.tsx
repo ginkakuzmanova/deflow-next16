9ebc0c5c4da4b779c8ced2dbacd730854fd61a9d
ginkapetrova <ginkakuzmanova@gmail.com>
2026-01-12 16:13:04 +0200

    feat: create Auth Layout & Social Auth Form

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main>
+            <Navbar />
+            {children}
+        </main>
+    )
+}
+export default RootLayout
