a8ef48d58357d9a27bfb93f72de9951a15f76c47
ginkapetrova <ginkakuzmanova@gmail.com>
2026-01-14 00:40:36 +0200

    feat: sso with github

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,47 +2,49 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import ThemeProvider from "@/context/Theme";
+import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const inter = localFont({
-    src: './fonts/InterVF.ttf',
-    variable: '--font-inter',
-    weight: '100 200 300 400 500 600 700 800 900'
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
 const spaceGrotesk = localFont({
-    src: './fonts/SpaceGroteskVF.ttf',
-    variable: '--font-space-grotesk',
-    weight: '300 400 500 600 700'
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
-    title: "De Overflow",
-    description:
-        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-    icons: {
-        icon: "/images/site-logo.svg",
-    },
+  title: "De Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-      <ThemeProvider
-        attribute={'class'}
-        defaultTheme={'system'}
-        enableSystem
-        disableTransitionOnChange
-      >
-          {children}
-      </ThemeProvider>
-      </body>
+      <SessionProvider session={session}>
+        <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+          <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
 }
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
+"use client"
+import React from 'react';
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {toast} from "sonner";
+import {signIn} from "next-auth/react";
+import ROUTES from "@/constants/routes";
 
 const SocialAuthForm = () => {
     const buttonClass = 'background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5';
+    const handleSignIn = async (provider: 'github' | 'google') => {
+        try {
+            await signIn(provider, {
+                redirectTo: ROUTES.HOME
+            });
+        } catch (error) {
+            console.log(error);
+            toast.error(
+                "Sign-In Failed",
+                { description: error instanceof Error
+                        ? error.message
+                        : "An error occurred during sign-in",
+                });
+        }
+    };
+
     return (
         <div className={'mt-10 flex flex-wrap gap-2.5'}>
-            <Button className={buttonClass}>
+            <Button className={buttonClass} onClick={() => handleSignIn('github')}>
                 <Image src={'/icons/github.svg'} alt="Github icon"
                     width={20} height={20} className={'invert-colors mr-2.5 object-contain'}
                 />
                 <span>Log in with GitHub</span>
             </Button>
-            <Button className={buttonClass}>
+            <Button className={buttonClass} onClick={() => handleSignIn('google')}>
                 <Image src={'/icons/google.svg'} alt="Google icon"
                        width={20} height={20} className={'mr-2.5 object-contain'}
                 />
