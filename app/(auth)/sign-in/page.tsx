905634a5bd35bb1f095c5de225e9db1ede01328e
ginkapetrova <ginkakuzmanova@gmail.com>
2026-01-14 08:23:53 +0200

    feat: auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+import { FieldValues } from "react-hook-form";
 
 const SignIn = () => {
-    return (
-        <div>SignIn</div>
-    )
-}
-export default SignIn
+  return (
+    <AuthForm
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data: FieldValues): Promise<{ success: boolean; data: FieldValues }> =>
+        Promise.resolve({ success: true, data })
+      }
+      formType={"SIGN_IN"}
+    />
+  );
+};
+export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
+import AuthForm from "@/components/forms/AuthForm";
+import { FieldValues } from "react-hook-form";
+import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
-    return (
-        <div>SignUp</div>
-    )
-}
-export default SignUp
+  return (
+    <AuthForm
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data: FieldValues): Promise<{ success: boolean; data: FieldValues }> =>
+        Promise.resolve({ success: true, data })
+      }
+      formType={"SIGN_UP"}
+    />
+  );
+};
+export default SignUp;
