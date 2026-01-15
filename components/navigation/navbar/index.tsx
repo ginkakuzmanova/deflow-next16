8a072167b5f5d7c8e089582df5d42f1566b70207
ginkapetrova <ginkakuzmanova@gmail.com>
2026-01-16 00:31:43 +0200

    feat: mobile nav

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 import Theme from "./Theme";
+import MobileNavigation from "@/components/navigation/navbar/MobileNavigation";
 
 const Navbar = () => {
-    return (
-        <nav
-            className="flex-between background-light900_dark200 fixed z-50 w-full
-             gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-            <Link href="/" className="flex items-center gap-1">
-                <Image
-                    src="/images/site-logo.svg"
-                    width={23}
-                    height={23}
-                    alt={"DeFlow logo"}
-                ></Image>
+  return (
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/images/site-logo.svg" width={23} height={23} alt={"DeFlow logo"}></Image>
 
-                <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-                    De<span className={'text-primary-500'}>Flow</span>
-                </p>
-            </Link>
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          De<span className={"text-primary-500"}>Flow</span>
+        </p>
+      </Link>
 
-            <p>Global Search</p>
-            <div className={'flex-between gap-5'}>
-                <Theme />
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+      <p>Global Search</p>
+      <div className={"flex-between gap-5"}>
+        <Theme />
+
+        <MobileNavigation />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
