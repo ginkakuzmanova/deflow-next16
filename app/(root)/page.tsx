df3f8072e62a18ffc9d1ffc785cc6707af0e2bdf
ginkapetrova <ginkakuzmanova@gmail.com>
2026-01-20 08:04:43 +0200

    feat: local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const Page = () => {
-    return (
-        <>
-            <div className={'text-light-500'}>WELCOME</div>
-            <div className={'h1-bold font-space-grotesk'}>WELCOME</div>
-        </>
+const Page = async ({ searchParams }: RouteParams) => {
+  const { query = "" } = await searchParams;
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900 font-space-grotesk">All Questions</h1>
 
-    )
-}
-export default Page
+        <Button className={"primary-gradient !text-light-900 min-h-[46px] px-4 py-3"} asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.HOME}
+          imgSrc={"/icons/search.svg"}
+          placeholder={"Search questions..."}
+          iconPosition={"left"}
+          otherClasses={"flex-1"}
+        />
+      </section>
+
+      {/*  COMP HOME FILTER*/}
+
+      <div className={"mt-10 flex w-full flex-col gap-6"}>
+        <p>Question 1 card based on the filters</p>
+        <p>Question 2 card</p>
+        <p>Question 3 card</p>
+      </div>
+    </>
+  );
+};
+export default Page;
---
+++ lib/url.ts
@@ -0,0 +1,28 @@
+import qs from "query-string";
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const queryString = qs.parse(params);
+
+  queryString[key] = value;
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: queryString,
+  });
+};
+
+export const removeKeysFromUrlQuery = ({ params, keysToRemove }: RemoveUrlQueryParams) => {
+  const queryString = qs.parse(params);
+
+  keysToRemove.forEach((key) => {
+    delete queryString[key];
+  });
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: queryString,
+    },
+    { skipNull: true }
+  );
+};
