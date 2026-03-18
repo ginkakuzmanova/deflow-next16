b83cb07521164660cd898034637baaf5ab179d91
ginkapetrova <ginkakuzmanova@gmail.com>
2026-03-18 17:40:57 +0200

    edit question action

---
+++ app/(root)/questions/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { notFound, redirect } from "next/navigation";
+import { auth } from "@/auth";
+import { getQuestion } from "@/lib/actions/question.action";
+import ROUTES from "@/constants/routes";
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const EditQuestion = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  if (!id) return notFound();
+
+  const session = await auth();
+  if (!session) return redirect("/sign-in");
+
+  const { data: question, success } = await getQuestion({ questionId: id });
+  if (!success) return notFound();
+
+  if (question?.author._id.toString() !== session.user?.id) return redirect(ROUTES.QUESTION(id));
+
+  return (
+    <main>
+      <QuestionForm question={question} isEdit />
+    </main>
+  );
+};
+export default EditQuestion;
